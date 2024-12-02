8baf257578a52747a5c89a21eac3a2cb40c19fc7
Rostislav <68556673+go-rostislav@users.noreply.github.com>
2024-12-02 23:42:50 +0100

    fix

--- src/components/pages/NotFound/NotFound.jsx
+++ src/components/pages/NotFound/NotFound.jsx
@@ -2,10 +2,9 @@ import "./NotFound.css";
 
 const NotFound = () => {
   return (
-    <div className="container page-not-found">
-      <h1>404</h1>
-      <h2>Page not found</h2>
-    </div>
+      <head>
+          <meta http-equiv="refresh" content="0; URL=./index.html"/>
+      </head>
   );
 };
 
